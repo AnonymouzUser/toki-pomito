697494363b2128ae65afef5ea6507067f36bfac8
Brayan <mrx.source.mrx@gmail.com>
2026-02-19 15:51:57 -0500

    feat: testing some things for toki-pomito

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Badge } from "./components/ui/badge";
+import { Button } from "./components/ui/button";
 
-function App() {
-  const [count, setCount] = useState(0)
+console.log("Me he montado correctamente");
 
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        {import.meta.env.MODE === "development" ? (
+          <Badge className="bg-gray-800 w-25 fixed top-3 left-3 text-white">
+            Development
+          </Badge>
+        ) : null}
+        <Button className="bg-gray-800 w-10 fixed top-3 right-3 text-white">
+          STG
+        </Button>
+        <div className="h-screen w-2/3 flex flex-col justify-center items-center">
+          <div className="flex flex-row gap-1">
+            {/* Deep and Work time */}
+            <Button className="bg-gray-600 h-10 w-60 text-white">
+              Deep Work
+            </Button>
+            <Button className="bg-gray-600 h-10 w-60 text-white">
+              Break Time
+            </Button>
+          </div>
+          <p className="text-8xl font-bold m-10 text-white">25:00</p>
+          <div className="flex flex-col gap-2">
+            {/* Start/Pause and Reset Buttons */}
+            <Button className="bg-gray-600 h-10 w-60 text-white">
+              Start/Pause
+            </Button>
+            <Button className="bg-gray-600 h-10 w-60 text-white">Reset</Button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
-
-export default App
+  );
+};
