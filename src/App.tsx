94749ed32306670ed4ab6f7344f374943bf48be5
Brayan <mrx.source.mrx@gmail.com>
2026-02-19 18:34:11 -0500

    feat: change some colors and add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Badge } from "./components/ui/badge";
+import { Navbar } from "./components/Navbar";
 import { Button } from "./components/ui/button";
 
 console.log("Me he montado correctamente");
@@ -7,31 +7,22 @@ export const App = () => {
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
-        {import.meta.env.MODE === "development" ? (
-          <Badge className="bg-gray-800 w-25 fixed top-3 left-3 text-white">
-            Development
-          </Badge>
-        ) : null}
-        <Button className="bg-gray-800 w-10 fixed top-3 right-3 text-white">
-          STG
-        </Button>
+        <Navbar />
         <div className="h-screen w-2/3 flex flex-col justify-center items-center">
           <div className="flex flex-row gap-1">
             {/* Deep and Work time */}
-            <Button className="bg-gray-600 h-10 w-60 text-white">
-              Deep Work
-            </Button>
-            <Button className="bg-gray-600 h-10 w-60 text-white">
+            <Button className="h-10 w-60 text-black bg-white">Deep Work</Button>
+            <Button className="h-10 w-60 text-black bg-white">
               Break Time
             </Button>
           </div>
           <p className="text-8xl font-bold m-10 text-white">25:00</p>
           <div className="flex flex-col gap-2">
             {/* Start/Pause and Reset Buttons */}
-            <Button className="bg-gray-600 h-10 w-60 text-white">
+            <Button className="bg-white h-10 w-120 text-black">
               Start/Pause
             </Button>
-            <Button className="bg-gray-600 h-10 w-60 text-white">Reset</Button>
+            <Button className="bg-white h-10 w-120 text-black">Reset</Button>
           </div>
         </div>
       </div>
