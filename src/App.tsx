703ce76f3d90e09c69b50eeef46fc8531419eb96
Brayan <mrx.source.mrx@gmail.com>
2026-02-19 20:02:25 -0500

    feat: testing pomodoro logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
+import { useEffect, useRef, useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Button } from "./components/ui/button";
 
 console.log("Me he montado correctamente");
 
+const POMODORO_TIME = 1 * 60;
+
 export const App = () => {
+  // Variables
+  const [timeLeft, setTimeLeft] = useState<number>(POMODORO_TIME);
+  const [isRuning, setIsRunning] = useState<boolean>(false);
+
+  const intervalRef = useRef<number | null>(null);
+
+  // Counter logic
+  useEffect(() => {
+    if (!isRuning) return;
+
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((prev) => {
+        if (prev <= 1) {
+          clearInterval(intervalRef.current!);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalRef.current!);
+  }, [isRuning]);
+
+  // Handlers
+
+  const handleStart = () => setIsRunning(true);
+  const handlePause = () => setIsRunning(false);
+  const handleReset = () => {
+    setIsRunning(false);
+    setTimeLeft(POMODORO_TIME);
+  };
+
+  // Format to mm:ss
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+
+    return `${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+  };
+
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
@@ -16,13 +60,32 @@ export const App = () => {
               Break Time
             </Button>
           </div>
-          <p className="text-8xl font-bold m-10 text-white">25:00</p>
+          <p className="text-9xl font-bold m-10 text-white">
+            {formatTime(timeLeft)}
+          </p>
           <div className="flex flex-col gap-2">
             {/* Start/Pause and Reset Buttons */}
-            <Button className="bg-white h-10 w-120 text-black">
-              Start/Pause
+
+            <div className="flex flex-row gap-2">
+              <Button
+                className="bg-white h-10 w-60 text-black"
+                onClick={handleStart}
+              >
+                Start
+              </Button>
+              <Button
+                className="bg-white h-10 w-60 text-black"
+                onClick={handlePause}
+              >
+                Pause
+              </Button>
+            </div>
+            <Button
+              className="bg-white h-10 w-120 text-black"
+              onClick={handleReset}
+            >
+              Reset
             </Button>
-            <Button className="bg-white h-10 w-120 text-black">Reset</Button>
           </div>
         </div>
       </div>
